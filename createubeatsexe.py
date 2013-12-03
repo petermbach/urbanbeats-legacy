129a88e4651bb3602331cf7a2a541cefaa3c7b42
Peter M Bach <peterbach@gmail.com>
2013-12-03 13:06:23 +1100

    modifications to the .exe creation script

--- createubeatsexe.py
+++ createubeatsexe.py
@@ -8,5 +8,7 @@ Created on Mon Aug 06 23:50:37 2012
 from distutils.core import setup
 import py2exe
 
-setup(windows=['main.pyw'],options = {"py2exe": {"dll_excludes": ["MSVCP90.dll"]}})
+setup(windows=['main.pyw'],
+      options = {
+          "py2exe": {"dll_excludes": ["MSVCP90.dll"], "includes":["sip", "PyQt4.QtNetwork"]}})
 #python createubeatsexe.py py2exe --includes sip
