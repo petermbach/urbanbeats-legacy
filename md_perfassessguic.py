dbdca7b862b3a5db65f64f3740fb68ed49e8f53e
Peter M Bach <peterbach@gmail.com>
2014-10-07 11:44:09 +1100

    Added connected md_perfassessguic file, now links dialog window with main UrbanBEATS GUI.

--- md_perfassessguic.py
+++ md_perfassessguic.py
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from md_perfassessgui import Ui_Perfconfig_Dialog
 from PyQt4 import QtGui, QtCore
+import os
 
 class PerfAssessGUILaunch(QtGui.QDialog):
     def __init__(self, activesim, tabindex, parent = None):
@@ -38,55 +39,97 @@ class PerfAssessGUILaunch(QtGui.QDialog):
         #----------------------------------------------------------------------#
         #-------- SELECT ANALYSES ---------------------------------------------#
         #----------------------------------------------------------------------#
+        self.ui.perf_MUSIC.setChecked(self.module.getParameter("perf_MUSIC"))
+        self.ui.perf_Economics.setChecked(self.module.getParameter("perf_Economics"))
+        self.ui.perf_Microclimate.setChecked(self.module.getParameter("perf_Microclimate"))
+        self.ui.perf_EPANET.setChecked(self.module.getParameter("perf_EPANET"))
+        self.ui.perf_CD3.setChecked(self.module.getParameter("perf_CD3"))
 
-        # self.ui.blocksize_in.setValue(int(self.module.getParameter("BlockSize")))
-        #
-        # if self.module.getParameter("blocksize_auto") == 1:
-        #     self.ui.blocksize_auto.setChecked(1)
-        #     self.ui.blocksize_in.setEnabled(0)
-        # else:
-        #     self.ui.blocksize_auto.setChecked(0)
-        #     self.ui.blocksize_in.setEnabled(1)
-        #
-        # QtCore.QObject.connect(self.ui.blocksize_auto, QtCore.SIGNAL("clicked()"), self.block_auto_size)
+        self.ed_MUSIC()
+
+        #self.ed_Economics()
+        #self.ed_Microclimate()
+        #self.ed_EPANET()
+        #self.ed_CD3()
+
+        QtCore.QObject.connect(self.ui.perf_MUSIC, QtCore.SIGNAL("clicked()"), self.ed_MUSIC)
+        # QtCore.QObject.connect(self.ui.perf_Economics, QtCore.SIGNAL("clicked()"), self.ed_Economics)
+        # QtCore.QObject.connect(self.ui.perf_Microclimate, QtCore.SIGNAL("clicked()"), self.ed_Microclimate)
+        # QtCore.QObject.connect(self.ui.perf_EPANET, QtCore.SIGNAL("clicked()"), self.ed_EPANET)
+        # QtCore.QObject.connect(self.ui.perf_CD3, QtCore.SIGNAL("clicked()"), self.ed_CD3)
 
         #----------------------------------------------------------------------#
         #-------- MUSIC -------------------------------------------------------#
         #----------------------------------------------------------------------#
+        self.versioncombo = ["Version 5", "Version 6"]
+        self.ui.music_version_combo.setCurrentIndex(self.versioncombo.index(self.module.getParameter("musicversion")))
 
-        pass
+        self.ui.music_browse_pathbox.setText(self.module.getParameter("musicclimatefile"))
+        QtCore.QObject.connect(self.ui.music_browse_button, QtCore.SIGNAL("clicked()"), self.perf_MUSIC_climatefile)
 
+        self.ui.musicsplit_check.setChecked(self.module.getParameter("musicseparatebasin"))
+        self.ui.musicBF_TN_box.setText(str(self.module.getParameter("bf_tncontent")))
+        self.ui.musicBF_ortho_box.setText(str(self.module.getParameter("bf_orthophosphate")))
+
+        self.ui.musicauto_check.setChecked(self.module.getParameter("musicautorun"))
+        QtCore.QObject.connect(self.ui.musicauto_check, QtCore.SIGNAL("clicked()"), self.ed_MUSIC)
+
+        self.ui.musicpath_box.setText(self.module.getParameter(str("musicpath")))
+        QtCore.QObject.connect(self.ui.musicpath_browse, QtCore.SIGNAL("clicked()"), self.perf_MUSIC_pathbrowse)
+
+        self.ui.musictte_check.setChecked(self.module.getParameter("musicTTE"))
+        self.ui.musicflux_check.setChecked(self.module.getParameter("musicFlux"))
 
         #----------------------------------------------------------------------#
         #-------- ECONOMICS ---------------------------------------------------#
         #----------------------------------------------------------------------#
 
-
-
-
         #----------------------------------------------------------------------#
         #-------- MICROCLIMATE ------------------------------------------------#
         #----------------------------------------------------------------------#
 
-
-
-
         #----------------------------------------------------------------------#
         #-------- EPANET ------------------------------------------------------#
         #----------------------------------------------------------------------#
-
-
 
         #----------------------------------------------------------------------#
         #-------- INTEGRATED WATER CYCLE MODEL  -------------------------------#
         #----------------------------------------------------------------------#
 
 
+        QtCore.QObject.connect(self.ui.buttonBox, QtCore.SIGNAL("accepted()"), self.save_values)
 
 
     def ed_MUSIC(self):
-        return True
+        self.ui.music_version_combo.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        self.ui.music_browse_pathbox.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        self.ui.music_browse_button.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        self.ui.musicsplit_check.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        self.ui.musicBF_TN_box.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        self.ui.musicBF_ortho_box.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        # self.ui.musicauto_check.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        self.ui.musicpath_box.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        self.ui.musicpath_browse.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        self.ui.musictte_check.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        self.ui.musicflux_check.setEnabled(int(self.ui.perf_MUSIC.isChecked()))
+        if self.ui.perf_MUSIC.isChecked():
+            self.ed_MUSICautorun()
 
+    def ed_MUSICautorun(self):
+        self.ui.musicpath_box.setEnabled(int(self.ui.musicauto_check.isChecked()))
+        self.ui.musicpath_browse.setEnabled(int(self.ui.musicauto_check.isChecked()))
+        self.ui.musictte_check.setEnabled(int(self.ui.musicauto_check.isChecked()))
+        self.ui.musicflux_check.setEnabled(int(self.ui.musicauto_check.isChecked()))
+
+    def perf_MUSIC_climatefile(self):
+        fname = QtGui.QFileDialog.getOpenFileName(self, "Locate Climate File .mlb...", os.curdir, str("Met Template (*.mlb)"))
+        if fname:
+            self.ui.music_browse_pathbox.setText(fname)
+
+    def perf_MUSIC_pathbrowse(self):
+        fname = QtGui.QFileDialog.getOpenFileName(self, "Locate MUSIC .exe...", os.curdir, str("Executable (*.exe)"))
+        if fname:
+            self.ui.musicpath_box.setText(fname)
 
     def ed_Economics(self):
         return True
@@ -109,41 +152,37 @@ class PerfAssessGUILaunch(QtGui.QDialog):
         #----------------------------------------------------------------------#
         #-------- SELECT ANALYSES ---------------------------------------------#
         #----------------------------------------------------------------------#
-        # self.module.setParameter("BlockSize", self.ui.blocksize_in.value())
-        # self.module.setParameter("blocksize_auto", int(int(self.ui.blocksize_auto.isChecked())))
+        self.module.setParameter("perf_MUSIC", int(self.ui.perf_MUSIC.isChecked()))
+        self.module.setParameter("perf_Economics", int(self.ui.perf_Economics.isChecked()))
+        self.module.setParameter("perf_Microclimate", int(self.ui.perf_Microclimate.isChecked()))
+        self.module.setParameter("perf_EPANET", int(self.ui.perf_EPANET.isChecked()))
+        self.module.setParameter("perf_CD3", int(self.ui.perf_CD3.isChecked()))
 
-         #----------------------------------------------------------------------#
+        #----------------------------------------------------------------------#
         #-------- MUSIC -------------------------------------------------------#
         #----------------------------------------------------------------------#
-
-
-
+        self.module.setParameter("musicversion", self.versioncombo[int(self.ui.music_version_combo.currentIndex())])
+        self.module.setParameter("musicclimatefile", self.ui.music_browse_pathbox.text())
+        self.module.setParameter("musicseparatebasin", int(self.ui.musicsplit_check.isChecked()))
+        self.module.setParameter("bf_tncontent", float(self.ui.musicBF_TN_box.text()))
+        self.module.setParameter("bf_orthophosphate", float(self.ui.musicBF_ortho_box.text()))
+        self.module.setParameter("musicautorun", int(self.ui.musicauto_check.isChecked()))
+        self.module.setParameter("musicpath", self.ui.musicpath_box.text())
+        self.module.setParameter("musicTTE", int(self.ui.musictte_check.isChecked()))
+        self.module.setParameter("musicFlux", int(self.ui.musicflux_check.isChecked()))
 
         #----------------------------------------------------------------------#
         #-------- ECONOMICS ---------------------------------------------------#
         #----------------------------------------------------------------------#
 
-
-
-
         #----------------------------------------------------------------------#
         #-------- MICROCLIMATE ------------------------------------------------#
         #----------------------------------------------------------------------#
-
-
-
 
         #----------------------------------------------------------------------#
         #-------- EPANET ------------------------------------------------------#
         #----------------------------------------------------------------------#
 
-
-
         #----------------------------------------------------------------------#
         #-------- INTEGRATED WATER CYCLE MODEL  -------------------------------#
         #----------------------------------------------------------------------#
-
-
-
-        pass
-        # self.emit(QtCore.SIGNAL("updatedDetails"))
