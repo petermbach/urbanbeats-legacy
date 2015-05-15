8cfbb2d01086b2e254e59bfd47669ae85637c57e
Peter M Bach <peterbach@gmail.com>
2015-05-15 15:43:14 +1000

    New include added to the py2exe script, recursion limit increased in script as well.

--- createubeatsexe.py
+++ createubeatsexe.py
@@ -7,12 +7,14 @@ Created on Mon Aug 06 23:50:37 2012
 
 from distutils.core import setup
 import py2exe
+import sys
+sys.setrecursionlimit(5000)
 
 setup(windows=[{ "script": "main.pyw",
                  "icon_resources": [(1, "ubeats.ico")],
                    }],
       options = {
-          "py2exe": {"dll_excludes": ["MSVCP90.dll"], "includes":["sip", "PyQt4.QtNetwork"]}})
+          "py2exe": {"dll_excludes": ["MSVCP90.dll"], "includes":["sip", "PyQt4.QtNetwork", "scipy.sparse.csgraph._validation"]}})
 
 #--- Run the script in the command line by navigating to the root folder of the software where main.pyw is
 #located and then type the following command... ---#
